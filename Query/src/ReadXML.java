cb56be8c4a8002ea7e132d079903c4b4aa386960
AlexQianYi <q563875735@gmail.com>
2018-07-23 16:22:11 +0800

    utility

--- Query/src/ReadXML.java
+++ Query/src/ReadXML.java
@@ -29,13 +29,21 @@ public class ReadXML {
 
         document = db.parse(fileName);
 
-        NodeList ruleList = document.getElementsByTagName("")
+        NodeList project_name_list = document.getElementsByTagName("project_name");
+        NodeList project_category_list = document.getElementsByTagName("project_category");
+        NodeList project_apartment_list = document.getElementsByTagName("project_apartment");
+        NodeList project_location_list = document.getElementsByTagName("project_location");
+        NodeList project_publish_time_list = document.getElementsByTagName("project_publish_time");
+        NodeList project_file_time_list = document.getElementsByTagName("project_file_time");
+        NodeList project_file_price_list = document.getElementsByTagName("project_file_price");
+        NodeList project_file_location_list = document.getElementsByTagName("project_file_location");
+        NodeList project_bit_time_list = document.getElementsByTagName("project_bit_time");
+        NodeList project_bit_location_list = document.getElementsByTagName("project_bit_location");
+        NodeList project_except_amount_list = document.getElementsByTagName("project_except_amount");
+        NodeList project_manager_list = document.getElementsByTagName("project_manager");
+        NodeList project_manager_tel_list = document.getElementsByTagName("project_manager_tel");
 
-        QueryRule rule = new QueryRule();
 
-
-
-        NodeList ruleList = document.getElementsByTagName()
 
     }
 
