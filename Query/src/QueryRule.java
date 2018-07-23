ab6740e2029cd3b5f4b5ac6797514a568f43db42
AlexQianYi <q563875735@gmail.com>
2018-07-23 15:49:34 +0800

    insert

--- Query/src/QueryRule.java
+++ Query/src/QueryRule.java
@@ -22,107 +22,92 @@ public class QueryRule {
         return this.project_name.length;
     }
 
-    public String getProject_name() {
-        return project_name;
+    public void setProject_name(int n, String content){
+        this.project_name[n] = content;
     }
 
-    public void setProject_name(String project_name) {
-        this.project_name = project_name;
+    public String getProject_name(int n){
+        return this.project_name[n];
     }
 
-    public String getProject_category() {
-        return project_category;
+    public void setProject_categorySize(int n){
+        this.project_category = new String[n];
     }
 
-    public void setProject_category(String project_category) {
-        this.project_category = project_category;
+    public int getProject_categorySize(){
+        return this.project_category.length;
     }
 
-    public String getProject_apartment() {
-        return project_apartment;
+    public void setProject_category(int n, String content){
+        this.project_category[n] = content;
     }
 
-    public void setProject_apartment(String project_apartment) {
-        this.project_apartment = project_apartment;
+    public String getProject_category(int n){
+        return this.project_category[n];
     }
 
-    public String getProject_location() {
-        return project_location;
+    public void setProject_apartmentSize(int n){
+        this.project_apartment = new String[n];
     }
 
-    public void setProject_location(String project_location) {
-        this.project_location = project_location;
+    public int getProject_apartmentSize(){
+        return this.project_apartment.length;
     }
 
-    public String getProject_publish_time() {
-        return project_publish_time;
+    public void setProject_apartment(int n, String content){
+        this.project_apartment[n] = content;
     }
 
-    public void setProject_publish_time(String project_publish_time) {
-        this.project_publish_time = project_publish_time;
+    public String getProject_apartment(int n){
+        return this.project_apartment[n];
     }
 
-    public String getProject_file_time() {
-        return project_file_time;
+    public void setProject_locationSize(int n){
+        this.project_location = new String[n];
     }
 
-    public void setProject_file_time(String project_file_time) {
-        this.project_file_time = project_file_time;
+    public int getProject_locationSize(){
+        return this.project_location.length;
     }
 
-    public String getProject_file_price() {
-        return project_file_price;
+    public void setProject_location(int n, String content){
+        this.project_location[n] = content;
     }
 
-    public void setProject_file_price(String project_file_price) {
-        this.project_file_price = project_file_price;
+    public String getProject_location(int n){
+        return this.project_location[n];
     }
 
-    public String getProject_file_location() {
-        return project_file_location;
+    public void setProject_publish_timeSize(int n){
+        this.project_publish_time = new String[n];
     }
 
-    public void setProject_file_location(String project_file_location) {
-        this.project_file_location = project_file_location;
+    public int getProject_publish_timeSize(){
+        return this.project_publish_time.length;
     }
 
-    public String getProject_bit_time() {
-        return project_bit_time;
+    public void setProject_publish_time(int n, String content){
+        this.project_publish_time[n] = content;
     }
 
-    public void setProject_bit_time(String project_bit_time) {
-        this.project_bit_time = project_bit_time;
+    public String getProject_pulish_time(int n){
+        return this.project_publish_time[n];
     }
 
-    public String getProject_bit_location() {
-        return project_bit_location;
+    public void setProject_file_timeSize(int n){
+        this.project_file_time = new String[n];
     }
 
-    public void setProject_bit_location(String project_bit_location) {
-        this.project_bit_location = project_bit_location;
+    public int getProject_file_timeSize(){
+        return this.project_file_time.length;
     }
 
-    public String getProject_except_amount() {
-        return project_except_amount;
+    public void setProject_file_time(int n, String content){
+        this.project_file_time[n] = content;
     }
 
-    public void setProject_except_amount(String project_except_amount) {
-        this.project_except_amount = project_except_amount;
+    public String getProject_file_time(int n){
+        return this.project_file_time[n];
     }
 
-    public String getProject_manager() {
-        return project_manager;
-    }
-
-    public void setProject_manager(String project_manager) {
-        this.project_manager = project_manager;
-    }
-
-    public String getProject_manager_tel() {
-        return project_manager_tel;
-    }
-
-    public void setProject_manager_tel(String project_manager_tel) {
-        this.project_manager_tel = project_manager_tel;
-    }
 }
