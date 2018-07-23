7905fab38763e88c83cefc2aadabdeff062a2252
AlexQianYi <q563875735@gmail.com>
2018-07-23 15:32:39 +0800

    query

---
+++ Query/src/QueryRule.java
@@ -0,0 +1,128 @@
+public class QueryRule {
+
+    private String[] project_name;
+    private String[] project_category;
+    private String[] project_apartment;
+    private String[] project_location;
+    private String[] project_publish_time;
+    private String[] project_file_time;
+    private String[] project_file_price;
+    private String[] project_file_location;
+    private String[] project_bit_time;
+    private String[] project_bit_location;
+    private String[] project_except_amount;
+    private String[] project_manager;
+    private String[] project_manager_tel;
+
+    public void setProject_nameSize(int n){
+        this.project_name = new String[n];
+    }
+
+    public int getProject_nameSize(){
+        return this.project_name.length;
+    }
+
+    public String getProject_name() {
+        return project_name;
+    }
+
+    public void setProject_name(String project_name) {
+        this.project_name = project_name;
+    }
+
+    public String getProject_category() {
+        return project_category;
+    }
+
+    public void setProject_category(String project_category) {
+        this.project_category = project_category;
+    }
+
+    public String getProject_apartment() {
+        return project_apartment;
+    }
+
+    public void setProject_apartment(String project_apartment) {
+        this.project_apartment = project_apartment;
+    }
+
+    public String getProject_location() {
+        return project_location;
+    }
+
+    public void setProject_location(String project_location) {
+        this.project_location = project_location;
+    }
+
+    public String getProject_publish_time() {
+        return project_publish_time;
+    }
+
+    public void setProject_publish_time(String project_publish_time) {
+        this.project_publish_time = project_publish_time;
+    }
+
+    public String getProject_file_time() {
+        return project_file_time;
+    }
+
+    public void setProject_file_time(String project_file_time) {
+        this.project_file_time = project_file_time;
+    }
+
+    public String getProject_file_price() {
+        return project_file_price;
+    }
+
+    public void setProject_file_price(String project_file_price) {
+        this.project_file_price = project_file_price;
+    }
+
+    public String getProject_file_location() {
+        return project_file_location;
+    }
+
+    public void setProject_file_location(String project_file_location) {
+        this.project_file_location = project_file_location;
+    }
+
+    public String getProject_bit_time() {
+        return project_bit_time;
+    }
+
+    public void setProject_bit_time(String project_bit_time) {
+        this.project_bit_time = project_bit_time;
+    }
+
+    public String getProject_bit_location() {
+        return project_bit_location;
+    }
+
+    public void setProject_bit_location(String project_bit_location) {
+        this.project_bit_location = project_bit_location;
+    }
+
+    public String getProject_except_amount() {
+        return project_except_amount;
+    }
+
+    public void setProject_except_amount(String project_except_amount) {
+        this.project_except_amount = project_except_amount;
+    }
+
+    public String getProject_manager() {
+        return project_manager;
+    }
+
+    public void setProject_manager(String project_manager) {
+        this.project_manager = project_manager;
+    }
+
+    public String getProject_manager_tel() {
+        return project_manager_tel;
+    }
+
+    public void setProject_manager_tel(String project_manager_tel) {
+        this.project_manager_tel = project_manager_tel;
+    }
+}
---
+++ Query/src/ReadXML.java
@@ -0,0 +1,38 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ReadXML {
+
+    private static DocumentBuilderFactory dbFactory = null;
+    private static DocumentBuilder db = null;
+    private static Document document = null;
+    private static List<QueryRule> queryRules = null;
+
+    static{
+        try{
+            dbFactory = DocumentBuilderFactory.newInstance();
+            db = dbFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<QueryRule> getQueryRules(String fileName) throws IOException, SAXException {
+
+        document = db.parse(fileName);
+
+        NodeList ruleList = document.getElementsByTagName()
+
+
+    }
+
+
+}
