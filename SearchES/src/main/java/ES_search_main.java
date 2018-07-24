42661434e213d9c60110bdc2faa6882becf3aea1
AlexQianYi <q563875735@gmail.com>
2018-07-24 15:06:30 +0800

    search

--- SearchES/src/main/java/ES_search_main.java
+++ SearchES/src/main/java/ES_search_main.java
@@ -1,18 +1,22 @@
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class ES_search_main {
 
-    public static void main(String[] args) throws IOException, SAXException {
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
 
         ES_utility utility = new ES_utility();
         TransportClient client = utility.getClient();
 
         ReadXML readXML = new ReadXML(client);
         String file_path = ES_utility.getSearchXML_file();
-        readXML.getQueryRules(file_path);
+        QueryBuilder qb = readXML.getQueryRules2(file_path);
+
+        readXML.Search(qb, "original_project_file", "doc");
     }
 }
